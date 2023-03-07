590f6d8b523a02a4f831d8222be91cc4c8e25d36
higcorlsourt <m211046@fmu.ac.jp>
2023-03-07 09:57:09 +0000

    first commit

---
+++ led.py
@@ -0,0 +1,27 @@
+# micro python code
+ 
+import machine
+import utime
+import random
+
+red    = machine.Pin(13, machine.Pin.OUT)
+green  = machine.Pin(12, machine.Pin.OUT)
+yellow = machine.Pin(14, machine.Pin.OUT)
+colors = [red, green, yellow]
+
+
+for i in range(1000):
+#    led = random.choice(colors)
+    led = colors[i%3]
+    
+    a = random.uniform(0,   0.15)
+    b = random.uniform(0.15, 0.3)
+    sleep_time = a + b
+    
+    led.value(1)
+    utime.sleep(0.15)
+ #    utime.sleep(sleep_time * random.random())
+    
+    led.value(0)
+    utime.sleep(0.15)
+#    utime.sleep(sleep_time * random.random())

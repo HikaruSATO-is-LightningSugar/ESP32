7b0bf32bfd014e637ef16cfcd0a29d17c062da9d
higcorlsourt <m211046@fmu.ac.jp>
2023-04-11 12:58:14 +0000

    ステップモーターの制御に、MED-PCが交流電流を出力する問題のパッチを適用

--- main_SmartCTL2stepper.py
+++ main_SmartCTL2stepper.py
@@ -10,12 +10,15 @@ moter_io_led_pin = 15
 SmartCTL_pin = 18
 tactswitch_pin = 21
 STOPswitch_pin = 19
+# asyncio setting
+TTL_pin = 12
 
 #from stepper_arduino import Stepper
 from stepper_qiita import Stepper
 import time
 import utime
 import machine
+import uasyncio
 
 
 # ステッピングモーターの初期設定
@@ -119,5 +122,40 @@ while True:
     elif the_number_of_requests < 0:
         the_number_of_requests = 0
         
+        
+        
+# MED-PCのSmartCTLを
+# SG-231の「28V VDC to TTL ADAPTOR」につないだら、
+# なぜか交流電流が流れており、
+# TTLの５Vの入力を取り込んで割り込みハンドラーを
+# 呼び出すつもりだったのと、
+# この入力をタクトスイッチを共有することで、
+# 回路として、MED-PC　＝　タクトスイッチON
+# の対応を実装するつもりだったのだが、
+# 今の所、できそうにない
+#
+# したがって、応急処置的に、
+# 非同期処理として、条件処理をする
+# SmartCTLがOFFの時：250.0 ~ 280 mV
+# SmartCTLがONの時：160 ~ 190 mV
+# の電圧を、ESP32が検出しているので、
+# これで、条件処理をして、
+# 割り込みハンドラ呼び出しの代わりとする
+# 他にもっといい方法があれば、そっちを使うと思う
+async_cycle_ms = 200 
+async def check_dc_nv(dc, period_ms):
+    global dc
+    while True:
+        val = dc.read_uv()
+        if val < 190000:
+            MEDPC_callback()
+        elif val > 240000:
+        await uasyncio.sleep_ms(period_ms)
+            
+async def main(pin, period_ms):
+    dc_pin = machine.Pin(pin, machine.Pin.IN)
+    dc = machine.ADC(dc_pin)
+    uasyncio.create_task(check_dc_nv(dc, period_ms))
+    await uasyncio.sleep_ms(period_ms)
     
-
+uasyncio.run(main(12, async_cycle_ms)
